7ad03eafbdceb402e2a6f10cc4c841cb884c0b4b
Carrot-Eater <zerry30089@gmail.com>
2022-12-06 18:17:37 +0900

    버그

--- lib/src/pages/upload.dart
+++ lib/src/pages/upload.dart
@@ -1,76 +1,32 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_instagram_clone/src/components/image_data.dart';
+import 'package:flutter_instagram_clone/src/controller/upload_controller.dart';
 import 'package:get/get.dart';
 import 'package:photo_manager/photo_manager.dart';
 import 'dart:typed_data';
 
-class Upload extends StatefulWidget {
-  const Upload({Key? key}) : super(key: key);
-
-  @override
-  State<Upload> createState() => _UploadState();
-}
-
-class _UploadState extends State<Upload> {
-  var albums = <AssetPathEntity>[];
-  var imageList = <AssetEntity>[];
-  var headerTitle = '';
-  AssetEntity? selectedImage;
-
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    _loadPhotos();
-  }
-
-  void _loadPhotos() async {
-    var result = await PhotoManager.requestPermissionExtend();
-    if (result.isAuth) {
-      albums = await PhotoManager.getAssetPathList(
-        type: RequestType.image,
-        filterOption: FilterOptionGroup(
-          imageOption: const FilterOption(
-            sizeConstraint: SizeConstraint(minHeight: 100, minWidth: 100),
-          ),
-          orders: [
-            const OrderOption(type: OrderOptionType.createDate, asc: false),
-          ],
-        ),
-      );
-      _loadData();
-    } else {}
-  }
-
-  void _loadData() async {
-    headerTitle = albums.first.name;
-    await _pagingPhotos();
-    update();
-  }
-
-  Future<void> _pagingPhotos() async {
-    var photos = await albums.first.getAssetListPaged(page: 0, size: 30);
-    imageList.addAll(photos);
-    selectedImage = imageList.first;
-  }
-
-  void update() => setState(() {});
+class Upload extends GetView<UploadController> {
+  Upload({Key? key}) : super(key: key);
 
   Widget _imagePreview() {
-    var width = MediaQuery.of(context).size.width;
-    return Container(
-      width: width,
-      height: width,
-      color: Colors.grey,
-      child: selectedImage == null
-          ? Container()
-          : _photoWidget(selectedImage!, width.toInt(), builder: (data) {
-              return Image.memory(
-                data,
-                fit: BoxFit.cover,
-              );
-            }),
+    var width = Get.width;
+    return Obx(
+      () => Container(
+        width: width,
+        height: width,
+        color: Colors.grey,
+        child: _photoWidget(
+          controller.selectedImage!.value,
+          width.toInt(),
+          builder: (data) {
+            return Image.memory(
+              data,
+              fit: BoxFit.cover,
+            );
+          },
+        ),
+      ),
     );
   }
 
@@ -83,21 +39,22 @@ class _UploadState extends State<Upload> {
           GestureDetector(
             onTap: () {
               showModalBottomSheet(
-                context: context,
+                context: Get.context!,
                 shape: const RoundedRectangleBorder(
                   borderRadius: BorderRadius.only(
                       topLeft: Radius.circular(20),
                       topRight: Radius.circular(20)),
                 ),
-                isScrollControlled: albums.length > 10 ? true : false,
+                isScrollControlled:
+                    controller.albums.length > 10 ? true : false,
                 constraints: BoxConstraints(
-                  maxHeight: MediaQuery.of(context).size.height -
-                      MediaQuery.of(context).padding.top,
+                  maxHeight: MediaQuery.of(Get.context!).size.height -
+                      MediaQuery.of(Get.context!).padding.top,
                 ),
                 builder: (_) => Container(
-                  height: albums.length > 10
+                  height: controller.albums.length > 10
                       ? Size.infinite.height
-                      : albums.length * 60,
+                      : controller.albums.length * 60,
                   child: Column(
                       crossAxisAlignment: CrossAxisAlignment.stretch,
                       children: [
@@ -117,11 +74,11 @@ class _UploadState extends State<Upload> {
                             child: Column(
                               crossAxisAlignment: CrossAxisAlignment.stretch,
                               children: List.generate(
-                                albums.length,
+                                controller.albums.length,
                                 (index) => Container(
                                   padding: const EdgeInsets.symmetric(
                                       vertical: 15, horizontal: 20),
-                                  child: Text(albums[index].name),
+                                  child: Text(controller.albums[index].name),
                                 ),
                               ),
                             ),
@@ -136,7 +93,7 @@ class _UploadState extends State<Upload> {
               child: Row(
                 children: [
                   Text(
-                    headerTitle,
+                    controller.headerTitle,
                     style: const TextStyle(
                       color: Colors.black,
                       fontSize: 18,
@@ -193,19 +150,23 @@ class _UploadState extends State<Upload> {
           childAspectRatio: 1,
           mainAxisSpacing: 1,
           crossAxisSpacing: 1),
-      itemCount: imageList.length,
+      itemCount: controller.imageList.length,
       itemBuilder: (BuildContext context, int index) {
-        return _photoWidget(imageList[index], 200, builder: (data) {
+        return _photoWidget(controller.imageList[index], 200, builder: (data) {
           return GestureDetector(
             onTap: () {
-              selectedImage = imageList[index];
-              update();
+              controller.changeSelectedImage(controller.imageList[index]);
             },
-            child: Opacity(
-              opacity: imageList[index] == selectedImage ? 0.3 : 1,
-              child: Image.memory(
-                data,
-                fit: BoxFit.cover,
+            child: Obx(
+              () => Opacity(
+                opacity: controller.imageList[index] ==
+                        controller.selectedImage!.value
+                    ? 0.3
+                    : 1,
+                child: Image.memory(
+                  data,
+                  fit: BoxFit.cover,
+                ),
               ),
             ),
           );
